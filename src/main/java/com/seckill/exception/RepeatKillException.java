be7f6b07628f954370d6e64ce2ac53f2de45fc09
Administrator <15060335532@139.com>
2018-09-13 00:50:01 +0800

    service设计

---
+++ src/main/java/com/seckill/exception/RepeatKillException.java
@@ -0,0 +1,15 @@
+package com.seckill.exception;
+/**
+ * 重发秒杀异常(运行期异常)
+ *
+ *
+ * */
+public class RepeatKillException extends RuntimeException{
+    public RepeatKillException(String message) {
+        super(message);
+    }
+
+    public RepeatKillException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/com/seckill/service/SeckillService.java
+++ src/main/java/com/seckill/service/SeckillService.java
@@ -1,7 +1,43 @@
 package com.seckill.service;
+
+import com.seckill.dto.Exposer;
+import com.seckill.dto.SeckillExecution;
+import com.seckill.entity.Seckill;
+
+import java.util.List;
+
 /**
- *
+ *方法定义粒度，参数，返回类型(return 类型/异常)
  * @author myfloweryourgrass
  * */
 public interface SeckillService {
+    /**
+     * 查询所有的秒杀记录
+     * @return
+     *
+     * */
+    List<Seckill> getSeckillList();
+
+    /**
+     * 查询单个秒杀记录
+     * @param seckillId
+     * @return
+     * */
+    Seckill getById(long seckillId);
+
+    /**
+     *
+     * 秒杀开启是输出秒杀接口地址，
+     * 否则输出系统时间和秒杀时间
+     * @param seckillId
+     * */
+    Exposer exportSeckillUrl(long seckillId);
+    /**
+     * 执行秒杀操作
+     * @param seckillId
+     * @param md5
+     * @param userPhone
+     * */
+
+    SeckillExecution executeSeckill(long seckillId, long userPhone, String md5)throws Exception;
 }
