9d03215cbc0aaffe8ae1c327c46ccba424dd92ff
lmm <15060335532@139.com>
2018-09-13 18:20:17 +0800

    service层修改

--- src/main/java/com/seckill/controller/SeckillController.java
+++ src/main/java/com/seckill/controller/SeckillController.java
@@ -11,7 +11,6 @@ import com.seckill.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
